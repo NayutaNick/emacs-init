a319735b81eb9921061ec2f9b961ac8a819ea7b6
Atuyan <kcinatuyan@lunarove.org>
2024-10-19 12:54:37 +0800

    Add centered-window-mode; bind <f11> <f12> for switching windows

--- init.el
+++ init.el
@@ -26,7 +26,6 @@
 ;; 设置包管理源
 (require 'package)
 (setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
                          ("melpa"  . "https://melpa.org/packages/")))
 (package-initialize)
 
@@ -39,12 +38,85 @@
 ;; ---------------------------
 ;; 界面美化
 ;; ---------------------------
+
+;; 隐藏菜单栏
+(menu-bar-mode -1)
+
 (use-package doom-themes
   :config (load-theme 'doom-one t))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+(defun centered-window-mode-enable ()
+  "Enable centered window mode when only one window is visible and there is no split."
+  (when (and (= (count-windows) 1)
+             (not (window-in-direction 'right)))
+    ;; Enable centering and width restriction
+    (setq-default left-margin-width (/ (- (window-total-width) 120) 2))
+    (setq-default right-margin-width (/ (- (window-total-width) 120) 2))
+    (set-window-buffer nil (current-buffer))
+    (message "Centered window mode enabled")))
+
+(defun centered-window-mode-disable ()
+  "Disable centered window mode and reset margins."
+  ;; Disable centering and reset margins
+  (setq-default left-margin-width 0)
+  (setq-default right-margin-width 0)
+  (set-window-buffer nil (current-buffer))
+  (message "Centered window mode disabled"))
+
+(defun toggle-centered-window ()
+  "Toggle centered window mode.
+Enable if currently disabled and only one window without splits;
+disable otherwise."
+  (interactive)
+  (if (bound-and-true-p centered-window-mode)
+      (progn
+        (centered-window-mode-disable)
+        (setq centered-window-mode nil))
+    (if (= (count-windows) 1)
+        (progn
+          (centered-window-mode-enable)
+          (setq centered-window-mode t))
+      (message "Centered window mode cannot be enabled with splits."))))
+
+(defun handle-window-configuration-change ()
+  "Disable centered window mode automatically when splits are present."
+  (if (> (count-windows) 1)
+        (progn
+        (centered-window-mode-disable)
+        (setq centered-window-mode nil))))
+
+;; Automatically handle window splitting or closing events
+(add-hook 'window-configuration-change-hook 'handle-window-configuration-change)
+
+(defun split-window-advice (orig-fun &rest args)
+  "Advice to disable centered window mode before splitting the window."
+  (when (bound-and-true-p centered-window-mode)
+    (centered-window-mode-disable)
+    (setq centered-window-mode nil))
+  (apply orig-fun args))
+
+;; Add advice to split-window (C-x 2 & 3)
+(advice-add 'split-window-below :around #'split-window-advice)
+(advice-add 'split-window-right :around #'split-window-advice)
+
+;; Bind to F10 key for toggling
+(global-set-key (kbd "<f10>") 'toggle-centered-window)
+
+;; Enable the mode by default if there is no horizontal split
+(centered-window-mode-enable)
+(setq centered-window-mode t)
 
 ;; ---------------------------
 ;; 编辑体验优化
 ;; ---------------------------
+;; F11: 跳转到上一个窗口
+(global-set-key (kbd "<f11>") (lambda () (interactive) (other-window -1)))
+;; F12: 跳转到下一个窗口
+(global-set-key (kbd "<f12>") (lambda () (interactive) (other-window 1)))
+
 (use-package ivy
   :ensure t
   :config
@@ -119,7 +191,7 @@
  '(custom-safe-themes
    '("8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default))
  '(package-selected-packages
-   '(dracula-theme flycheck-rust rust-mode racer markdown-mode doom-themes smartparens treemacs-projectile projectile treemacs flycheck company)))
+   '(gnu-elpa-keyring-update dracula-theme flycheck-rust rust-mode racer markdown-mode doom-themes smartparens treemacs-projectile projectile treemacs flycheck company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
